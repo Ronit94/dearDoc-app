7b6f46455b06ba5f895c1cc5213c25986cb8daf4
Ronit Sarma <ronnie.sarma94@gmail.com>
2021-12-04 20:10:06 +0530

    Page Not found component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UiStyleToggleService } from 'src/services/ui-style-toggle.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dearDoc-app';
-  public isActive:boolean = false
+  public isDark: boolean = false;
+  public isActive: boolean = false;
+  constructor(private ThemeService: UiStyleToggleService){}
+  ngOnInit(){
+    this.ThemeService.setThemeOnStart();
+  }
+
+  ToggleTheme() {
+    this.ThemeService.toggle()
+  }
 }
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -3,7 +3,6 @@ import {
   Component,
   OnInit,
 } from '@angular/core';
-import { UiStyleToggleService } from '../../services/ui-style-toggle.service';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
@@ -13,16 +12,12 @@ import { UiStyleToggleService } from '../../services/ui-style-toggle.service';
 
 
 export class LandingPageComponent implements OnInit {
-  constructor(private ThemeService: UiStyleToggleService) {}
-  public isDark: boolean = false;
-  public isActive: boolean = false;
+  constructor() {}
 
   ngOnInit(): void {
-    this.ThemeService.setThemeOnStart();
+    //this.ThemeService.setThemeOnStart();
   }
 
-  ToggleTheme() {
-    this.ThemeService.toggle()
-  }
+  
 
 }
